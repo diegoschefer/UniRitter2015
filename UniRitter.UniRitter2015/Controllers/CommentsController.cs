f4439ead7d86e3f086a002c870cca38c5ac73aca
Diego Schefer <diegoschefer@gmail.com>
2015-07-31 21:44:47 -0300

    Implements abstract controller and generic methods

--- UniRitter.UniRitter2015/Controllers/CommentsController.cs
+++ UniRitter.UniRitter2015/Controllers/CommentsController.cs
@@ -1,9 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web.Http;
+using UniRitter.UniRitter2015.Models;
+using UniRitter.UniRitter2015.Services;
+using UniRitter.UniRitter2015.Services.Implementation;
 
 namespace UniRitter.UniRitter2015.Controllers
 {
-    public class CommentsController : ApiController
+
+    public class PeopleController : BaseController<PersonModel>
     {
+        private readonly IRepository<PersonModel> _repo;
+
+        public PeopleController(IRepository<PersonModel> repo) : base(repo)
+        {
+            _repo = repo;
+        }
     }
+
 }
